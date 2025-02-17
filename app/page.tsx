8ed202773d557cc495e94e0a0c440f8c91aaa959
ZL <ray040406@gmail.com>
2025-02-18 01:33:37 +1300

    3d rotating animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,43 @@
 import Link from "next/link";
 import Image from "next/image";
 import Globe from "../public/assets/globe.svg";
+import styles from "../app/styles/Home.module.css";
 export default function Home() {
   return (
     <div className="">
       <h1 id="home-title" className=""></h1>
       <div className="ml-24 flex justify-center md:justify-normal md:text-2xl text-xl mt-4 md:mt-12">
-        <ul className="flex flex-col md:gap-3 gap-4">
-          <li className="group underline-offset-4">
-            <Link href="/about">
-              <span className="hover:underline">About</span>
-            </Link>
-            <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
-          </li>
-          <li className="group underline-offset-4">
-            <span className="hover:underline">Projects</span>{" "}
-            <span className="opacity-0 ml-4 group-hover:opacity-100">◂</span>
-          </li>
-          <li className="group underline-offset-4">
-            <span className="hover:underline">Blogs</span>{" "}
-            <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
-          </li>
-          <li className="group underline-offset-4">
-            <span className="hover:underline">Contact</span>{" "}
-            <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
-          </li>
-        </ul>
-        <Link href="/about">
-          <Image src={Globe} alt="globe_icon" width={50} height={50} />
-        </Link>
+        <div className="flex flex-col">
+          <ul className="flex flex-col md:gap-3 gap-4">
+            <li className="group underline-offset-4">
+              <Link href="/about">
+                <span className="hover:underline">About</span>
+              </Link>
+              <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
+            </li>
+            <li className="group underline-offset-4">
+              <span className="hover:underline">Projects</span>
+              <span className="opacity-0 ml-4 group-hover:opacity-100">◂</span>
+            </li>
+            <li className="group underline-offset-4">
+              <span className="hover:underline">Blogs</span>
+              <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
+            </li>
+            <li className="group underline-offset-4">
+              <span className="hover:underline">Contact</span>
+              <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
+            </li>
+          </ul>{" "}
+          <Link href="/" className="mt-12">
+            <Image
+              src={Globe}
+              alt="globe_icon"
+              width={30}
+              height={30}
+              className={styles.rotating}
+            />
+          </Link>
+        </div>
       </div>
     </div>
   );
