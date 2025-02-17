86d67fb0f666bda5975e823dcf011d0f92b429b1
ZL <ray040406@gmail.com>
2025-02-18 02:06:37 +1300

    x

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,9 @@
 const Footer = () => {
   return (
     <div>
-      <div className="border-t border-black w-full h-8">this is a footer</div>
+      <div className="border-t border-black w-full h-8">
+        This is a fake HTML + CSS website.{" "}
+      </div>
     </div>
   );
 };
