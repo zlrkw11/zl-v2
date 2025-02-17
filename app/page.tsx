2cbdab1533ec157a6d39bd735014d26ab7d8fb00
ZL <ray040406@gmail.com>
2025-02-17 19:45:57 +1300

    mobile nav

--- app/page.tsx
+++ app/page.tsx
@@ -3,23 +3,23 @@ export default function Home() {
   return (
     <div className="">
       <h1 id="home-title" className=""></h1>
-      <div className="ml-2 flex justify-center md:justify-normal">
-        <ul className="">
-          <li className="group relative underline-offset-4">
+      <div className="ml-2 flex justify-center md:justify-normal md:text-base text-lg mt-4 md:mt-0">
+        <ul className="flex flex-col md:gap-0 gap-4">
+          <li className="group underline-offset-4">
             <Link href="/about">
               <span className="hover:underline">About</span>
             </Link>
             <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
           </li>
-          <li className="group relative underline-offset-4">
+          <li className="group underline-offset-4">
             <span className="hover:underline">Projects</span>{" "}
             <span className="opacity-0 ml-4 group-hover:opacity-100">◂</span>
           </li>
-          <li className="group relative underline-offset-4">
+          <li className="group underline-offset-4">
             <span className="hover:underline">Blogs</span>{" "}
             <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
           </li>
-          <li className="group relative underline-offset-4">
+          <li className="group underline-offset-4">
             <span className="hover:underline">Contact</span>{" "}
             <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
           </li>
