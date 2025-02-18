fd50db1e0ff1d005096fbbb7124ffc4d6948885e
ZL <ray040406@gmail.com>
2025-02-18 16:12:40 +1300

    adjusted positioning

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Globe from "../public/assets/globe.svg";
 import styles from "../app/styles/Home.module.css";
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex ">
       <div className="md:ml-24 flex justify-center md:justify-normal md:text-2xl text-2xl mt-4 md:mt-12">
         <ul className="flex flex-col md:gap-3 md:text-left">
           <li className="group underline-offset-4">
@@ -24,22 +24,22 @@ export default function Home() {
           <li className="group underline-offset-4">
             <span className="hover:underline">Contact</span>
             <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
-          </li>
+          </li>{" "}
+          <div className="w-12 mt-8 md:mt-4">
+            <Link href="/" className="">
+              <Image
+                src={Globe}
+                alt="globe_icon"
+                width={30}
+                height={30}
+                className={`${styles.rotating}`}
+              />
+            </Link>
+          </div>
         </ul>{" "}
       </div>
-      <div className="w-full flex justify-center md:justify-normal">
-        <div className="w-12 mt-8 -translate-x-8 md:translate-x-0 md:mt-12 md:ml-24">
-          <Link href="/" className="">
-            <Image
-              src={Globe}
-              alt="globe_icon"
-              width={30}
-              height={30}
-              className={`${styles.rotating}`}
-            />
-          </Link>
-        </div>
-      </div>
+
+      <h1 className="flex ml-auto mr-24 mt-56">Welcome to my personal space</h1>
     </div>
   );
 }
