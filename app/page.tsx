495371b12af6462de5c8019768f133b85df933de
ZL <ray040406@gmail.com>
2025-02-19 16:09:46 +1300

    search bar

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import Bulb from "../public/assets/bulb.svg";
 import styles from "../app/styles/Home.module.css";
 export default function Home() {
   return (
-    <div className="flex ">
-      <div className="md:ml-24 flex justify-center md:justify-normal md:text-2xl text-2xl mt-4 md:mt-12">
-        <ul className="flex flex-col md:gap-3 md:text-left">
+    <div className="flex justify-center items-center ">
+      {/* <div className="md:ml-24 flex justify-center md:justify-normal md:text-2xl text-2xl mt-4 md:mt-12"> */}
+      {/* <ul className="flex flex-col md:gap-3 md:text-left">
           <li className="group underline-offset-4">
             <Link href="/about">
               <span className="hover:underline">About</span>
@@ -25,24 +25,31 @@ export default function Home() {
           <li className="group underline-offset-4">
             <span className="hover:underline">Contact</span>
             <span className="opacity-0 ml-4 group-hover:opacity-100 ">◂</span>
-          </li>{" "}
-          <div className="w-12 mt-8 md:mt-4">
-            <Link href="/" className="">
-              <Image
-                src={Globe}
-                alt="globe_icon"
-                width={30}
-                height={30}
-                className={`${styles.rotating}`}
-              />
-            </Link>
+          </li>{" "} */}
+      <div className="w-[800px] grid grid-cols-8 gap-4">
+        <div className="col-start-1 col-span-6 flex flex-col gap-4">
+          <h1 className="flex text-5xl">Welcome to my personal space.</h1>
+          <div className="flex gap-4">
+            <div className="flex border border-gray-300 rounded-lg p-2 ">
+              <p>direct me into zl's digital room</p>
+            </div>{" "}
+            <button className="">search</button>
           </div>
-        </ul>{" "}
+        </div>
+        {/* </ul>{" "} */}
+        {/* </div> */}{" "}
+        <div className="w-12 col-start-8 col-span-1 flex items-center justify-center">
+          <Link href="/" className="">
+            <Image
+              src={Globe}
+              alt="globe_icon"
+              width={30}
+              height={30}
+              className={`${styles.rotating}`}
+            />
+          </Link>
+        </div>{" "}
       </div>
-
-      <h1 className="flex ml-auto mr-24 mt-48 text-2xl">
-        Welcome to my personal space.
-      </h1>
     </div>
   );
 }
