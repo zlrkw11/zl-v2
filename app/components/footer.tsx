6c39b8feebfe0135441bd28c2e520c5c10781667
ZL <ray040406@gmail.com>
2025-02-20 01:44:33 +1300

    github on footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,8 +1,17 @@
 "use client";
+import Image from "next/image";
+import Link from "next/link";
+import github from "../../public/assets/gh.svg";
+
 const Footer = () => {
   return (
-    <div className="flex ml-auto m-2">
-      <div className=" w-full h-8 ">Copyright © 2025 zl</div>
+    <div className="flex ml-auto">
+      <div className="flex w-full h-8 mr-4 items-center gap-4 pb-4">
+        <Link href="https://github.com/zlrkw11">
+          <Image src={github} alt="github" width={30} height={30} />
+        </Link>
+        <p>Copyright © 2025 zl</p>
+      </div>
     </div>
   );
 };
