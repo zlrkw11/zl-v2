3a6355211d3822c8a8f9b0fe121fec547f277eb1
ZL <ray040406@gmail.com>
2025-02-20 16:18:45 +1300

    hover on nav links

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,22 +1,29 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+
+const isExpanded = () => {
+  const [expanded, setExpanded] = useState(false);
+  return expanded;
+};
 
 const Nav = () => {
   return (
     <div className="w-full grid grid-cols-8 items-center">
       <div className="col-start-8 col-span-4 flex justify-center md:text-xl text-lg gap-4 ">
-        <Link className="hover:underline hover:text-red-600" href="/">
+        <Link className="hover:underline hover:text-gray-700" href="/">
           Home
         </Link>
-        <Link className="hover:underline hover:text-red-600" href="/about">
+        <Link className="hover:underline hover:text-gray-600 " href="/about">
           About
         </Link>
-        <Link className="hover:underline hover:text-red-600" href="/">
+        <Link className="hover:underline hover:text-gray-600 " href="/">
           Projects
         </Link>
-        <Link className="hover:underline hover:text-red-600" href="/">
+        <Link className="hover:underline hover:text-gray-600 " href="/">
           Blogs
         </Link>{" "}
-        <Link className="hover:underline hover:text-red-600" href="/">
+        <Link className="hover:underline hover:text-gray-600" href="/">
           Contents
         </Link>
       </div>
