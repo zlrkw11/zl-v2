9506c7c7fe82474b94fd72ac0b99c62ff344720c
ZL <ray040406@gmail.com>
2025-02-24 13:35:26 +1300

    description changes

--- app/contents/page.tsx
+++ app/contents/page.tsx
@@ -18,7 +18,7 @@ const Card = ({
         <h1 className="hover:text-red-600 ">./{title}</h1>
       </Link>
 
-      <div>{description}</div>
+      <div className="">{description}</div>
     </div>
   );
 };
@@ -31,24 +31,28 @@ const Menu = () => {
       <div className="w-[800px] flex flex-col items-center gap-4">
         <h1 className="text-red-600 underline text-2xl mb-4">Map</h1>
         <p className="mb-8">This page is a road map for this website {`:)`}</p>
-        <h1 className="text-lg">./Ground_Level</h1>
+        <h1 className="text-lg text-black">./Ground_Level</h1>
         <Card title="Home" description="As what the name says.." />
         <Card
           title="About"
-          description="The page unprofessionally written by me, about me."
+          description="A page for the readers to know more about me and the background of this website."
         />
         <Card
           title="Projects"
-          description="Basically the repositories sitting on my github account."
+          description="Past projects (web-dev & others) I have built both independently and collaboratively."
         />
         <Card
           title="Personal Blogs"
           description="A free space to express my personal ideas and thoughts."
         />
-        <h1 className="text-lg">./B1</h1>
+        <h1 className="text-lg text-black">./B1</h1>
         <Card
           title="LeetCode"
           description="This page is an online storage room for all the problems I have solved."
+        />
+        <Card
+          title="Patch Notes"
+          description="A page which records all the updates and version changes of this website."
         />
       </div>
     </div>
