130c899b14e255a1be952d80277c29b3022ce4c5
ZL <ray040406@gmail.com>
2025-02-24 18:41:23 +1300

    w increase

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -19,7 +19,7 @@ const Blogs = async () => {
       className={`flex flex-col md:w-[800px] ${ArimoFont.className} items-center gap-4 text-gray-700`}
     >
       <h1 className="text-2xl text-red-600 underline mb-2">Blogs</h1>
-      <p className="mb-2 md:min-w-[600px] w-[420px]">
+      <p className="mb-2 md:min-w-[700px] w-[420px]">
         I'm not a person who regularly expresses their thoughts. But I do have a
         lot of them going on. I believe that thoughts are a valuable thing
         because they are always real. So, I made this page to write them down.
@@ -28,7 +28,7 @@ const Blogs = async () => {
         {blogs.map((blog: any) => (
           <li
             key={blog.slug}
-            className="p-2 md:min-w-[600px] w-[420px] border-2 border-l-red-600"
+            className="p-2 md:min-w-[700px] w-[420px] border-2 border-l-red-600"
           >
             <Link href={`/blogs/${blog.slug}`}>
               <p className="text-lg text-black hover:text-red-600">
