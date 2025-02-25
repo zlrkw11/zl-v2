238be6b5d1ce593ff7b8513c22248f83002a0c2f
ZL <ray040406@gmail.com>
2025-02-25 17:23:15 +1300

    blog header stlyling update

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -16,15 +16,19 @@ const Blogs = async () => {
 
   return (
     <div
-      className={`flex flex-col md:w-[800px] ${ArimoFont.className} items-center gap-4 text-gray-700`}
+      className={`flex flex-col md:w-[800px] items-center gap-4 text-gray-700`}
     >
-      <h1 className="text-2xl text-red-600 underline mb-2">Blogs</h1>
-      <p className="mb-2 md:min-w-[700px] w-[420px]">
+      <div className="md:min-w-[700px]">
+        <h1 className="text-3xl text-black border-b-red-600 border-b-2 mb-2 mr-auto">
+          Blogs:
+        </h1>
+      </div>
+      <p className={`mb-2 md:min-w-[700px] w-[420px] ${ArimoFont.className} `}>
         I'm not a person who regularly expresses their thoughts. But I do have a
         lot of them going on. I believe that thoughts are a valuable thing
         because they are always real. So, I made this page to write them down.
       </p>
-      <ul className="gap-2 flex flex-col">
+      <ul className={`gap-2 flex flex-col ${ArimoFont.className}`}>
         {blogs.map((blog: any) => (
           <li
             key={blog.slug}
