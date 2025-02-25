4e9753a1be15d908b5f47c040235c3337863ea90
ZL <ray040406@gmail.com>
2025-02-25 19:02:47 +1300

    expander

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { Arimo } from "next/font/google";
-import { list } from "postcss";
+import Link from "next/link";
+import { useState } from "react";
 
 const ArimoFont = Arimo({
   subsets: ["latin"],
@@ -21,37 +23,59 @@ const Project = ({
   image?: string;
   time: string;
 }) => {
+  const [expanded, setExpanded] = useState(false);
+  const toggle = () => {
+    setExpanded(!expanded);
+  };
   return (
-    <div
-      className={`md:min-w-[730px] ${ArimoFont.className} border-l-2 border-l-red-600 bg-gradient-to-r from-white to-gray-300`}
-    >
-      <h1 className="border-b-2 border-dashed border-b-red-600 text-black text-2xl m-2">
-        {name}
-      </h1>
-      <ul className="flex gap-2 m-2">
-        {tech.map((s, i) => (
-          <li
-            className="border border-gray-500 text-sm rounded-full p-1"
-            key={i}
-          >
-            {s}
-          </li>
-        ))}
-      </ul>
-      <p className="m-2">{des}</p>
-    </div>
+    <>
+      <div
+        className={`md:min-w-[730px] ${ArimoFont.className} border-l-2 border-l-red-600 bg-gradient-to-r from-white to-gray-200`}
+      >
+        <h1 className="border-b-2 border-dashed border-b-red-600 text-black text-2xl m-2">
+          {name}
+        </h1>
+        <ul className="flex gap-2 m-2">
+          {tech.map((s, i) => (
+            <li
+              className="border border-gray-500 text-sm rounded-full p-1"
+              key={i}
+            >
+              {s}
+            </li>
+          ))}
+        </ul>
+        <p className="m-2">{des}</p>
+
+        <div onClick={toggle}>expand</div>
+      </div>
+      <div
+        className={`${
+          ArimoFont.className
+        } transition-transform ml-auto border w-[200px] mb-4 duration-300 ${
+          expanded
+            ? "translate-y-0 opacity-100"
+            : "translate-y-[-50%] opacity-0"
+        } p-2 `}
+      >
+        <p>Github</p>
+        <span>
+          <Link href={link1}>WDCC/UASC</Link>
+        </span>
+      </div>
+    </>
   );
 };
 
 const Projects = () => {
   return (
-    <div className="flex flex-col md:w-[800px] items-center gap-4 text-gray-700">
+    <div className="flex flex-col md:w-[800px] items-center text-gray-700">
       <Project
         name="UASC"
         tech={["React", "TailwindCSS", "TypeScript", "Storybook"]}
         des="A fully functional website made for the University of Auckland's Snow Sports Club with a complete user-registration 
         and admin system that can handle large-scale online transfer and bookings with a professional procedure."
-        link1="uasc.co.nz"
+        link1="https://github.com/UoaWDCC/uasc-web"
         time="2024.4"
       />
     </div>
