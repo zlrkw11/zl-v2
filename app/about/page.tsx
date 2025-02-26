f4d2f4d5d984d10699bff5be9639080d6e02a167
ZL <ray040406@gmail.com>
2025-02-26 21:14:15 +1300

    styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -25,8 +25,11 @@ const About = () => {
             about me<span className="text-3xl text-red-600">:</span>
           </h1>
 
-          <div id="image-section">
-            <Image src={me} width={100} height={100} alt="a picture of sona" />
+          <div
+            id="image-section"
+            className="flex flex-col md:items-start items-center"
+          >
+            <Image src={me} width={200} height={200} alt="a picture of sona" />
             <p className={`text-gray-400 ${ArimoFont.className}`}>
               probably a selfie taken for LinkedIn
             </p>
