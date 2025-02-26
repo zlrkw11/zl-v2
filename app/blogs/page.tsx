d1cb142fb0d2c8fab5d91d17cb8ab2c6d59a2e4b
ZL <ray040406@gmail.com>
2025-02-26 23:45:18 +1300

    minor details

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -15,14 +15,14 @@ const Blogs = async () => {
   const blogs = JSON.parse(fileData);
 
   return (
-    <div className={`flex flex-col md:w-[800px] gap-2 text-gray-700 m-2`}>
+    <div className={`flex flex-col md:w-[800px] gap-4 text-gray-700 m-2 `}>
       <div className="md:min-w-[700px] flex flex-col justify-start">
-        <h1 className="text-3xl text-black border-b-red-600 border-b-2 mr-auto">
+        <h1 className="text-3xl md:min-w-[800px] text-black border-b-red-600 border-b-2 mr-auto">
           Blogs:
         </h1>
       </div>
 
-      <p className={`mb-2 md:max-w-[600px] ${ArimoFont.className} `}>
+      <p className={`mb-2 md:max-w-[800px] ${ArimoFont.className} `}>
         I'm not a person who regularly expresses their thoughts. But I do have a
         lot of them going on. I believe that thoughts are a valuable thing
         because they are always real. So, I made this page to write them down.
