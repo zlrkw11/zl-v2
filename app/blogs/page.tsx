61ee261da3f17dc54165406043fc6686944b7694
ZL <ray040406@gmail.com>
2025-02-26 19:17:40 +1300

    blog page change

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -16,25 +16,27 @@ const Blogs = async () => {
 
   return (
     <div
-      className={`flex flex-col md:w-[800px] max-w-[420px] items-center gap-4 text-gray-700`}
+      className={`flex flex-col md:w-[800px] max-w-[330px] jutify-center items-center gap-4 text-gray-700 `}
     >
-      <div className="md:min-w-[700px]">
-        <h1 className="text-3xl text-black border-b-red-600 border-b-2 mb-2 mr-auto">
+      <div className="md:min-w-[700px] max-w-[330px]">
+        <h1 className="text-3xl max-w-[330px] md:min-w-[700px] text-black border-b-red-600 border-b-2 mb-2 mr-auto">
           Blogs:
         </h1>
       </div>
       <p
-        className={`mb-2 md:min-w-[700px] m-2 text-sm md:text-base max-w-[350px] ${ArimoFont.className} `}
+        className={`mb-2 md:min-w-[700px] max-w-[300px] md:m-0 ${ArimoFont.className} `}
       >
         I'm not a person who regularly expresses their thoughts. But I do have a
         lot of them going on. I believe that thoughts are a valuable thing
         because they are always real. So, I made this page to write them down.
       </p>
-      <ul className={`gap-2 flex flex-col ${ArimoFont.className}`}>
+      <ul
+        className={`gap-2 flex flex-col ${ArimoFont.className} md:min-w-[700px] max-w-[350px]`}
+      >
         {blogs.map((blog: any) => (
           <li
             key={blog.slug}
-            className="p-2 md:min-w-[700px] max-w-[420px] border-2 border-l-red-600"
+            className="p-2 border-2 border-l-red-600 md:min-w-[700px] max-w-[350px]"
           >
             <Link href={`/blogs/${blog.slug}`}>
               <p className="text-lg text-black hover:text-red-600">
