16146a830940c9b4666175876326c2fa0ca3dc10
ZL <ray040406@gmail.com>
2025-02-26 23:47:08 +1300

    reduced not needed code

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -18,14 +18,14 @@ const Blog = async ({ params }: { params: Promise<{ slug: string }> }) => {
   }
   return (
     <div
-      className={`${ArimoFont.className} flex flex-col max-w-[420px] md:w-[800px] items-center text-gray-700 gap-4 m-2`}
+      className={`${ArimoFont.className} flex flex-col md:w-[800px] items-center text-gray-700 gap-4 m-2`}
     >
       <h1 className="text-lg border-b-2 border-b-red-600 text-black">
         {blog.title}
       </h1>
       <p className="text-gray-500">{blog.date}</p>
       <p
-        className="md:min-w-[700px] max-w-[420px]"
+        className="md:min-w-[700px]"
         dangerouslySetInnerHTML={{ __html: blog.text }}
       ></p>
     </div>
