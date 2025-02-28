61d03055e1e9506a34c406627c037b4ab2ed0387
ZL <ray040406@gmail.com>
2025-03-01 00:02:14 +1300

    styling title

--- app/contents/leetcode/[id]/page.tsx
+++ app/contents/leetcode/[id]/page.tsx
@@ -22,15 +22,23 @@ const ProblemPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   }
   return (
     <div
-      className={`md:w-[800px] m-2 flex flex-col gap-4 ${ArimoFont.className}`}
+      className={`md:w-[800px] m-2 flex flex-col gap-4 ${ArimoFont.className} items-center`}
     >
       <div className="flex gap-4">
-        <p>{problem.id}</p>
-        <h1>{problem.title}</h1>
+        <h1 className="gap-2 flex">
+          <p>{problem.id}</p>
+          <p>{problem.title}</p>
+        </h1>
         <h1
-          className={`${problem.difficulty === "easy" && `text-green-300`}${
-            problem.difficulty === "medium" && `text-orange-300`
-          }${problem.difficulty === "hard" && `text-red-600`}`}
+          className={`${
+            problem.difficulty === "easy"
+              ? "text-green-500"
+              : problem.difficulty === "medium"
+              ? "text-orange-500"
+              : problem.difficulty === "hard"
+              ? "text-red-600"
+              : ""
+          }`}
         >
           {problem.difficulty}
         </h1>
