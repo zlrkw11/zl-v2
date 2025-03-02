e90c9490bce2d1ba4f1c1152132a943780a4dd0f
ZL <ray040406@gmail.com>
2025-03-02 15:21:18 +1300

    code constant

--- data/problems.ts
+++ data/problems.ts
@@ -1,11 +1,26 @@
 import { LeetCodeProps as problem } from "@/app/types/types";
+export const CODE_WRAPPER = (code: string) => `
+  <pre style="
+    background-color: #e5e7e9;
+    color: #1E1E1E;
+    padding: 1rem;
+    border-radius: 8px;
+    overflow-x: auto;
+    white-space: pre-wrap;
+    font-family: monospace;
+    ">
+<code>${code}</code>
+  </pre>
+`;
+
 export const problems: problem[] = [
   {
     id: 1,
     title: "Two Sum",
     difficulty: "easy",
     label: ["array", "string"],
-    texts: ``,
+    texts: CODE_WRAPPER(`
+     `),
   },
   {
     id: 4,
