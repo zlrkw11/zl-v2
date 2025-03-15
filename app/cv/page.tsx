f76304073f0f101b0b4140461b6c8594bdd9a2a1
ZL <ray040406@gmail.com>
2025-03-15 14:10:53 +1300

    name update

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -2,7 +2,7 @@ const cv = () => {
   return (
     <div>
       <iframe
-        src="/Ray_Zhao_cv.pdf"
+        src="/ray_zhao_cv.pdf"
         className="w-screen h-screen"
         title="cv viewer"
       />
