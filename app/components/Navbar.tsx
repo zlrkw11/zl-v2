d5cf6923f157d16c1bd47ddd870a0de9daeeec46
ZL <ray040406@gmail.com>
2025-04-16 00:56:05 +1200

    fix: added turbopack for fast loading

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,7 +36,7 @@ export default function Navbar() {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white duration-200 focus:ring-2 focus:ring-gray-300 focus:outline-hidden focus:ring-inset">
+            <DisclosureButton className="group relative inline-flex items-center ml-4 justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white duration-200 focus:ring-2 focus:ring-gray-300 focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
