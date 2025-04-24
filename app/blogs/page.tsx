bfa10a62d009cf059dac7bc94026c197d3598bf6
ZL <ray040406@gmail.com>
2025-04-24 12:42:47 +1200

    feat: patch notes + blogs page dark mode applied

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -16,15 +16,17 @@ const Blogs = async () => {
 
   return (
     <div
-      className={`flex flex-col md:w-[800px] gap-4 text-gray-700 m-2 animate-slide-down`}
+      className={`flex flex-col md:w-[800px] gap-4 text-gray-700  m-2 animate-slide-down`}
     >
       <div className="md:min-w-[700px] flex flex-col justify-start">
-        <h1 className="text-3xl md:min-w-[800px] text-black border-b-red-600 border-b-2 mr-auto">
+        <h1 className="text-3xl md:min-w-[800px] text-black border-b-red-600 dark:text-neutral-300 border-b-2 mr-auto">
           Blogs:
         </h1>
       </div>
 
-      <p className={`mb-2 md:max-w-[800px] ${ArimoFont.className} `}>
+      <p
+        className={`mb-2 md:max-w-[800px] ${ArimoFont.className} dark:text-neutral-400`}
+      >
         I'm not a person who regularly expresses their thoughts. But I do have a
         lot of them going on. I believe that thoughts are a valuable thing
         because they are always real. So, I made this page to write them down.
@@ -36,10 +38,10 @@ const Blogs = async () => {
         {blogs.map((blog: any) => (
           <li
             key={blog.slug}
-            className="transition hover:translate-x-5 duration-300 p-2 border border-l-4 border-r-2 rounded-r-lg border-l-red-600 bg-gray-100 bg-opacity-60 md:min-w-[700px]"
+            className="transition dark:bg-neutral-600 hover:translate-x-5 duration-300 p-2 border border-l-4 rounded-r-lg border-l-red-600 bg-gray-100 bg-opacity-60 md:min-w-[700px]"
           >
             <Link href={`/blogs/${blog.slug}`}>
-              <p className="text-lg text-gray-700 hover:text-red-600">
+              <p className="text-lg text-gray-700 hover:text-red-600 dark:hover:text-neutral-800 dark:text-neutral-300">
                 {blog.title}
               </p>
             </Link>
--- app/contents/credits/page.tsx
+++ app/contents/credits/page.tsx
@@ -22,28 +22,28 @@ const credits = () => {
       className={`md:w-[800px] m-4 w-[370px] gap-4 items-center flex flex-col ${ArimoFont.className}`}
     >
       <h1
-        className={`${crimsonFont.className} text-2xl border-b-2 border-b-red-600 mr-auto`}
+        className={`${crimsonFont.className} text-2xl border-b-2 border-b-red-600 mr-auto dark:text-neutral-300`}
       >
         Dev
       </h1>
-      <p className="mb-2 text-lg">
+      <p className="mb-2 text-lg dark:text-neutral-400">
         This website was designed, developed and deployed completely
         independently by{" "}
         <span className="text-red-600 hover:text-red-400">Ray Zhao</span> in Feb
         2025 and is completely open source.
       </p>
       <h1
-        className={`${crimsonFont.className} text-2xl border-b-2 border-b-red-600 mr-auto`}
+        className={`${crimsonFont.className} text-2xl border-b-2 border-b-red-600 mr-auto dark:text-neutral-300`}
       >
         Inspirations
       </h1>
-      <p className="text-lg">
+      <p className="text-lg dark:text-neutral-400">
         I drew a lot of design inspiration from Eric's website, which I
         discovered while working on mine. His creative use of HTML and CSS to
         build a unique, professional site sparked several ideas for my own
         design.
       </p>
-      <p className={`text-2xl ${crimsonFont.className}`}>
+      <p className={`text-2xl ${crimsonFont.className} dark:text-neutral-400`}>
         Check out Eric's website!
       </p>
       <Link
