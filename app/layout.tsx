10d64142c7f685074209b2610f89b8c258f38e69
ZL <ray040406@gmail.com>
2025-08-08 00:49:51 +1200

    feat: styling scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
         <link rel="icon" href="/assets/zlnew.ico" sizes="any" />
       </head>
       <body
-        className={`min-h-screen flex flex-col ${crimsonFont.className} overflow-x-hidden [&::-webkit-scrollbar]:w-2
-      [&::-webkit-scrollbar-track]:bg-gray-100
+        className={`min-h-screen flex flex-col ${crimsonFont.className} overflow-x-hidden [&::-webkit-scrollbar]:w-2 
+      [&::-webkit-scrollbar-track]:bg-gray-600
       [&::-webkit-scrollbar-thumb]:bg-gray-300
-      dark:[&::-webkit-scrollbar-track]:bg-neutral-700
-      dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500`}
+      dark:[&::-webkit-scrollbar-track]:bg-red-600
+      dark:[&::-webkit-scrollbar-thumb]:bg-gray-300`}
       >
         <div className="flex items-center justify-center w-full mb-4">
           <Navbar />
